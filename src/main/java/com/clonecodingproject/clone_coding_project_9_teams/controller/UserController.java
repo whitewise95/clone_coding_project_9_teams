1daf519c0e45b59f94b01abc506cd4752c3ca69b
KorOnechild <kyuhong99@gmail.com>
2022-06-20 16:54:21 +0900

    UserController/UserRepository 수정 - Login 관련 내용 추가
    (cherry picked from commit 8b590e9c1c352181217ad03b337289734a5c3563)

--- src/main/java/com/clonecodingproject/clone_coding_project_9_teams/controller/UserController.java
+++ src/main/java/com/clonecodingproject/clone_coding_project_9_teams/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.clonecodingproject.clone_coding_project_9_teams.controller;
 
+import com.clonecodingproject.clone_coding_project_9_teams.dto.LoginDto;
+import com.clonecodingproject.clone_coding_project_9_teams.dto.LoginResDto;
 import com.clonecodingproject.clone_coding_project_9_teams.dto.SignupDto;
 import com.clonecodingproject.clone_coding_project_9_teams.dto.SignupResDto;
-import com.clonecodingproject.clone_coding_project_9_teams.service.UserService;
+import com.clonecodingproject.clone_coding_project_9_teams.service.user.LoginService;
+import com.clonecodingproject.clone_coding_project_9_teams.service.user.SignupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +18,23 @@ import java.security.NoSuchAlgorithmException;
 @RestController
 public class UserController {
 
-    private final UserService userService;
+    private final SignupService userService;
+    private final LoginService loginService;
 
+    //회원가입 요청 처리
     @PostMapping("/user/signup")
-    public SignupResDto signUp(@RequestBody SignupDto signupDto) throws NoSuchAlgorithmException {
+    public SignupResDto signUp(@RequestBody SignupDto signupDto) throws Exception {
         return userService.signUp(signupDto);
+    }
+
+    //로그인 요청
+    @PostMapping("/user/login")
+    public LoginResDto login(@RequestBody LoginDto loginDto) throws Exception {
+        return loginService.login(loginDto);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "ok";
     }
 }
