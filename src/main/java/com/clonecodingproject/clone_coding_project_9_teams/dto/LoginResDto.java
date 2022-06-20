3825f9a90583b9f54383ae7a3248c08b8f80abee
KorOnechild <kyuhong99@gmail.com>
2022-06-20 16:57:17 +0900

    Dto 추가 -LoginDto(Request) -LoginResDto(Response)
    (cherry picked from commit db2bdc2565b713408641e2a8a7616ca0b9ead524)

---
+++ src/main/java/com/clonecodingproject/clone_coding_project_9_teams/dto/LoginResDto.java
@@ -0,0 +1,14 @@
+package com.clonecodingproject.clone_coding_project_9_teams.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class LoginResDto {
+    private String nickname;
+    private String region;
+    private String accessToken;
+    private String errorMessage;
+
+}
