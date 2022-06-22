3857483329977465f3a2a7ef9d88f035d781c6cc
KorOnechild <kyuhong99@gmail.com>
2022-06-22 10:53:13 +0900

    Service 부분 예외처리 로직 변경
    (cherry picked from commit dce38e025c2d6d3d79065603aabd8a9fcb6b4e8f)

--- src/main/java/com/clonecodingproject/clone_coding_project_9_teams/service/user/LoginService.java
+++ src/main/java/com/clonecodingproject/clone_coding_project_9_teams/service/user/LoginService.java
@@ -2,12 +2,15 @@ package com.clonecodingproject.clone_coding_project_9_teams.service.user;
 
 import com.clonecodingproject.clone_coding_project_9_teams.domain.User;
 import com.clonecodingproject.clone_coding_project_9_teams.dto.*;
+import com.clonecodingproject.clone_coding_project_9_teams.error.ErrorCode;
+import com.clonecodingproject.clone_coding_project_9_teams.error.exceptions.LoginException;
 import com.clonecodingproject.clone_coding_project_9_teams.jwt.JwtTokenProvider;
 import com.clonecodingproject.clone_coding_project_9_teams.jwt.encoder.SHA256;
 import com.clonecodingproject.clone_coding_project_9_teams.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -17,14 +20,14 @@ public class LoginService {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
 
-    public LoginResDto login(LoginDto loginDto) throws Exception {
+    public LoginResDto login(LoginDto loginDto) throws NoSuchAlgorithmException {
 
         //로그인 정보에 맞는 유저 찾기
         Optional<User> user = userRepository.findByUsernameAndPassword(loginDto.getUsername(), SHA256.encrypt(loginDto.getPassword()));
 
         //아이디 비번 틀렸을 시
         if (!user.isPresent()) {
-            throw new IllegalArgumentException("아이디 또는 비밀번호가 틀렸습니다.");
+            throw new LoginException("이메일 또는 패스워드가 틀렸습니다.", ErrorCode.LOGIN_EXCEPTION);
         }
 
         String accessToken = jwtTokenProvider.createAccessToken(user.get().getUsername());
--- src/main/java/com/clonecodingproject/clone_coding_project_9_teams/service/user/SignupService.java
+++ src/main/java/com/clonecodingproject/clone_coding_project_9_teams/service/user/SignupService.java
@@ -3,11 +3,16 @@ package com.clonecodingproject.clone_coding_project_9_teams.service.user;
 import com.clonecodingproject.clone_coding_project_9_teams.domain.User;
 import com.clonecodingproject.clone_coding_project_9_teams.dto.SignupDto;
 import com.clonecodingproject.clone_coding_project_9_teams.dto.SignupResDto;
+import com.clonecodingproject.clone_coding_project_9_teams.error.ErrorCode;
+import com.clonecodingproject.clone_coding_project_9_teams.error.exceptions.EmailDubplicateException;
+import com.clonecodingproject.clone_coding_project_9_teams.error.exceptions.NicknameDubplicateException;
 import com.clonecodingproject.clone_coding_project_9_teams.jwt.encoder.SHA256;
 import com.clonecodingproject.clone_coding_project_9_teams.repository.UserRepository;
 import com.clonecodingproject.clone_coding_project_9_teams.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
 
 @RequiredArgsConstructor
 @Service
@@ -15,18 +20,18 @@ public class SignupService {
 
     private final UserRepository userRepository;
     private final UserValidator userValidator;
-    public SignupResDto signUp(SignupDto signupDto) throws Exception {
+    public SignupResDto signUp(SignupDto signupDto) throws NoSuchAlgorithmException {
         SignupResDto signupResDto = new SignupResDto();
         //입력 받은 값 체크
         userValidator.checkValues(signupDto);
 
         //등록된 유저 중 이메일 중복 체크
         if(userValidator.checkDupeEmail(signupDto)){
-            throw new IllegalArgumentException("이메일이 중복되었습니다.");
+            throw new EmailDubplicateException("이메일이 중복되었습니다.", ErrorCode.EMAIL_DUBP_EXCEPTION);
 
         //등록된 유저 중 닉네임 중복 체크
         }else if (userValidator.checkDupeNickname(signupDto)) {
-            throw new IllegalArgumentException("닉네임이 중복 되었습니다.");
+            throw new NicknameDubplicateException("닉네임이 중복되었습니다.", ErrorCode.NICKNAME_DUBP_EXCEPTION);
 
         //위의 모든 예외처리를 통과하면 모든 비밀번호 암호화 후 유저 등록
         }else{
